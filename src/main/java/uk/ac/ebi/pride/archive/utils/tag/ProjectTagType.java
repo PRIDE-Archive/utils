11d242e5b197df1ca1427cebb15997afcdb11ffe
Noemi del Toro Ayllón <noedelta@users.noreply.github.com>
2014-10-29 15:06:56 +0000

    Move archive-utils and archive-data-provider-api to pride-archive

---
+++ src/main/java/uk/ac/ebi/pride/archive/utils/tag/ProjectTagType.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.pride.archive.utils.tag;
+
+/**
+ * Type of the project tags
+ *
+ * @author Rui Wang
+ * @version $Id$
+ */
+public enum ProjectTagType {
+    PARENT_PROJECT,
+    PRIDE_INTERNAL,
+    PRIDE_HIGHLIGHTED
+}
